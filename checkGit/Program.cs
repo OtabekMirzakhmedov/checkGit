e7ca4e1c4bfe48499172a18ae14875f217f007ba
ITRANSITION\o.mirzakhmedov <o.mirzakhmedov@itransition.com>
2025-02-06 19:44:52 +0500

    changes in master

--- checkGit/Program.cs
+++ checkGit/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, Master");
+Console.WriteLine("Hello, Master Branch!");
+Console.WriteLine("Hello, Feature Branch!");
