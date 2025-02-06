680e525e2ad521c8bd8c484b65bbc1828d0b86c2
ITRANSITION\o.mirzakhmedov <o.mirzakhmedov@itransition.com>
2025-02-06 19:59:47 +0500

    asdf

--- checkGit/Program.cs
+++ checkGit/Program.cs
@@ -4,4 +4,5 @@ Console.WriteLine("Hello, Master Branch!");
 Console.WriteLine("Hello, Feature Branch!");
 Console.WriteLine("Hello, feature1");
 Console.WriteLine("Hello, feature2 from master to rebase");
+Console.WriteLine("Hello, feature2 again from master to rebase");
 ;
